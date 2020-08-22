86199b29f699a013f90293748f5d09d042ba2608
agro cembeliq <cembeliq@gmail.com>
2020-08-22 23:05:26 +0700

    modif appdownload

--- src/components/AppDownload.vue
+++ src/components/AppDownload.vue
@@ -5,13 +5,25 @@
       <h3 class="mb-3">Download app</h3>
 
       <a href="#">
-        <img src="images/misc/appstore.png" height="40" />
+        <img :src=appstore height="40" />
       </a>
       <a href="#">
-        <img src="images/misc/playmarket.png" height="40" />
+        <img :src=playmarket height="40" />
       </a>
     </div>
     <!-- container // -->
   </section>
   <!-- ========================= SECTION  END// ======================= -->
 </template>
+
+<script>
+export default {
+  name: 'my-component',
+    data () {
+      return {
+        appstore : process.env.VUE_APP_URL_ROOT+"images/misc/appstore.png",
+        playmarket : process.env.VUE_APP_URL_ROOT+"images/misc/playmarket.png"
+      }
+    }
+}
+</script>
